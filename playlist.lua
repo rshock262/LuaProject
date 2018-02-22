8c8f3b9336ff9f74ec7d79cd395ac508c92bdebd
Robert Shockley <rshockle@uncc.edu>
2018-02-22 16:30:50 +0000

    New branch for possibly breaking stuff

---
+++ playlist.lua
@@ -0,0 +1,7 @@
+--[[
+	Playlist where songs are objects that have name, artist, and album parameters as strings
+	Playlist itself is a an array of each object
+	Necessary playlist features: Play, add, remove	Possible extras: Search, update, multiple playlists
+--]]v
+
+function 
