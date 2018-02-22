5dce23d4a3a51b014cd445bda301f411ed5dd241
Robert Shockley <rshockle@uncc.edu>
2018-02-22 16:38:51 +0000

    Started playlist file

--- playlist.lua
+++ playlist.lua
@@ -1,7 +1,16 @@
 --[[
 	Playlist where songs are objects that have name, artist, and album parameters as strings
 	Playlist itself is a an array of each object
-	Necessary playlist features: Play, add, remove	Possible extras: Search, update, multiple playlists
---]]v
+	Necessary playlist features: Play, add, remove
+	Possible extras: Search, update, multiple playlists
+--]]
 
-function 
+Song = {"N/A"}
+
+function Song:new (o, name)
+	o = o or {name=name}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
